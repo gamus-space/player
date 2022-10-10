47a03a1146f7babb774d74a9115478480f929a59
Krzysztof Kostałkowicz <12763939+krzykos@users.noreply.github.com>
2022-10-10 23:36:26 +0200

    Support MUS

--- utils/serve.js
+++ utils/serve.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(express.static('..'));
 
-app.get('*', function(req, res){
+app.use((req, res, next) => {
+	res.status(404);
 	res.sendFile(`${process.cwd()}/index.html`);
 });
 
