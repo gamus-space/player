b7e8b3e7d819b117156cc8887b03de6c83fc939b
Krzysztof Kostałkowicz <12763939+krzykos@users.noreply.github.com>
2025-02-22 22:46:06 +0100

    Fix routing bug

--- src/common.js
+++ src/common.js
@@ -1,6 +1,6 @@
 export function customEncodeURIComponent(str) {
 	return str.replace(/ /g, '_').replace(
-		/[^/_\w():&"'\.,!\+\-]/g,
+		/[^_\w():&"'\.,!\+\-]/g,
 		(c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`,
 	);
 }
